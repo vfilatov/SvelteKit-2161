d0b444057f078f39dcdccb77522a639bc7b56b06
Val Filatov <valeriy_filatov2@comcast.com>
2021-08-11 13:06:14 -0400

    make it fail in browser

--- src/routes/__layout.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import Icon from 'svelte-awesome';
-  import { spinner as mySpinner} from 'svelte-awesome/icons';
-</script>
-
-<h1>Header</h1>
-<hr />
-<slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,8 @@
-<p>Hello, World!!!</p>
+<script>
+  import Icon from 'svelte-awesome';
+  import { spinner as mySpinner} from 'svelte-awesome/icons';
+</script>
+
+<h1>Header</h1>
+<hr />
+<p>Hello, World</p>
